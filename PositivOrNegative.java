86e7f9a62dcb54a1ac4d3790f6c93b763bac64e4
saranya2192 <103879563+saranya2192@users.noreply.github.com>
2022-04-17 16:23:16 +0530

    Add files via upload

---
+++ PositivOrNegative.java
@@ -0,0 +1,23 @@
+package Assignment1.day1;
+
+public class PositivOrNegative {
+	public static void main(String[] args)
+	{
+		int a = -35;
+		if(a>0)
+		{
+			System.out.println("the no is positve");
+					}
+		if(a<0)
+		{
+			System.out.println("the no is negative");
+		}
+		if(a==0)
+		{
+			System.out.println("the no is either positive or negative");
+		}
+	}
+	
+	
+
+}
---
+++ PositiveNumber.java
@@ -0,0 +1,14 @@
+package Assignment1.day1;
+
+public class PositiveNumber {
+	public static void main(String[] args) {
+		int a=-15;
+		if(a<=0)
+		{
+			System.out.println("The number -15 negative is converted to +15");
+				
+	}
+		
+	}
+
+}
